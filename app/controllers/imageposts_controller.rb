75d574a009fa7ec78a833823908d9ac3fe97b69d
Akhil Ramesh <akkhil@outlook.com>
2015-10-07 19:00:33 +0530

    image controller

--- app/controllers/imageposts_controller.rb
+++ app/controllers/imageposts_controller.rb
@@ -4,4 +4,27 @@ class ImagepostsController < ApplicationController
     @images = ImagePost.all
     render json: @images, status: 200
   end
+
+  def create
+    @image = ImagePost.new(imagepost_params)
+    if @image.save!
+      render json: @image, status: 200
+    else
+      render json: @image.errors, status: 422
+    end
+  end
+
+  def destroy
+    @image = ImagePost.find(params[:id])
+    if @image.destroy!
+      render text: "Image was deleted", status: 200
+    else
+      render status: 422
+    end
+  end
+
+  private
+    def imagepost_params
+      params.require(:image).permit(:image, :user_id)
+    end
 end
