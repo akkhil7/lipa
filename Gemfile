d68b601eca56782b1f4aeae9f5feb7ee68f99b0f
Akhil Ramesh <akkhil@outlook.com>
2015-11-03 22:09:25 +0530

    added annotate

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem "paperclip", "~> 4.3"
 gem 'rack-cors', :require => 'rack/cors'
 group :development, :test do
   gem 'byebug'
+  gem 'annotate'  
   gem 'web-console', '~> 2.0'
   gem 'spring'
 end
--- test/models/image_post_test.rb
+++ test/models/image_post_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: image_posts
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  image      :attachment
+#  user_id    :integer
+#
+
 require 'test_helper'
 
 class ImagePostTest < ActiveSupport::TestCase
